efe4d1279ce8ce231f05bb409ff00326e6ce4700
Yarviz <juzzzzi@hotmail.com>
2022-03-27 12:36:37 +0300

    Done exercises 2.9 and 2.10

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,10 +1,58 @@
 import { useState } from 'react'
 
+const Filter = ({value, cb}) => {
+  return (
+    <div>
+      filter person:
+        <input
+          value={value}
+          onChange={cb}
+        />
+    </div>
+  )
+}
+
+const DataForm = ({data, onSubmit}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      {data.map(item => {
+        return(
+          <div key={item.text}>
+            {item.text}
+            <input
+              value={item.value}
+              onChange={item.cb}
+            />
+          </div>
+        )
+      })}
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({persons}) => {
+  const list_persons = persons.map(person =>
+    <div key={person.name}>{person.name} {person.phone}</div>
+  )
+  return(
+    <div>{list_persons}</div>
+  )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123123' }
-  ])
-  const [newPerson, setNewPerson] = useState({ name: '', phone: '' })
+  const test_persons = [
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]
+
+  const [persons, setPersons] = useState(test_persons);
+  const [newPerson, setNewPerson] = useState({ name: '', phone: '' });
+  const [filter, setFilter] = useState('');
 
   const nameChange = (event) => {
     setNewPerson({...newPerson, name: event.target.value});
@@ -12,6 +60,10 @@ const App = () => {
 
   const phoneChange = (event) => {
     setNewPerson({...newPerson, phone: event.target.value});
+  }
+
+  const filterChange = (event) => {
+    setFilter(event.target.value);
   }
 
   const addPerson = (event) => {
@@ -24,37 +76,24 @@ const App = () => {
     setNewPerson({ name: '', phone: '' });
   }
 
+  const filtered_persons = persons.filter(
+    person => person.name.toLowerCase().startsWith(filter.toLowerCase())
+  );
+
+  const person_data = [
+    {text: "name:", value: newPerson.name, cb: nameChange},
+    {text: "phone:", value: newPerson.phone, cb: phoneChange},
+  ]
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name:
-          <input
-            value={newPerson.name}
-            onChange={nameChange}
-          />
-        </div>
-        <div>
-          number:
-          <input
-            value={newPerson.phone}
-            onChange={phoneChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter values={filter} cb={filterChange}/>
+      <DataForm data={person_data} onSubmit={addPerson}/>
       <h2>Numbers</h2>
-      <list>
-        {persons.map(person =>
-          <li>{person.name} {person.phone}</li>
-        )}
-      </list>
+      <Persons persons={filtered_persons}/>
     </div>
   )
-
 }
 
 export default App
