e72480a3c79d34fda11b1e5123968ae50b0cfab5
Yarviz <juzzzzi@hotmail.com>
2022-04-01 20:59:01 +0300

    Exercises 2.19 and 2.20 done

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import server from './services/persons'
 import DataForm from './components/DataForm'
 import Persons from './components/Persons'
+import './styles.css'
 
 const Filter = ({value, cb}) => {
   return (
@@ -15,10 +16,21 @@ const Filter = ({value, cb}) => {
   )
 }
 
+const Message = ({message}) => {
+  if (!message) return(<></>);
+  const style = message.is_error ? 'error' : 'ok';
+  return(
+    <div className={style}>
+      {message.msg}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newPerson, setNewPerson] = useState({ name: '', number: '' });
   const [filter, setFilter] = useState('');
+  const [message, setMessage] = useState(null);
 
   const nameChange = (event) => {
     setNewPerson({...newPerson, name: event.target.value});
@@ -30,6 +42,12 @@ const App = () => {
 
   const filterChange = (event) => {
     setFilter(event.target.value);
+  }
+
+  const setMsg = (msg, is_error) => {
+    console.log(msg)
+    setMessage({msg, is_error});
+    setTimeout(() => setMessage(null), 2000);
   }
 
   const addPerson = (event) => {
@@ -46,26 +64,29 @@ const App = () => {
           const index = new_list.map(item => item.id).indexOf(person.id);
           new_list[index] = updated;
           setPersons(new_list);
+          setMsg(`${newPerson.name} updated`, false);
           setNewPerson({ name: '', number: '' });
-        }).catch(reason => alert(reason));
+        }).catch(() => setMsg(`failed to update ${newPerson.name} to server`, true));
         return;
       }
     }
     server.create(newPerson).then(data => {
       setPersons(persons.concat(data));
+      setMsg(`Added ${newPerson.name} to phonebook`, false);
       setNewPerson({ name: '', number: '' });
       console.log(data);
-    }).catch(reason => alert(reason));
+    }).catch(() => setMsg(`failed to add ${newPerson.name} to server`, true));
   }
 
   const deletePerson = (person) => {
     if (window.confirm(`Delete ${person.name}?`)) {
       server.remove(person.id).then(() => {
+        setMsg(`${person.name} removed`, false);
         const new_list = [...persons];
         const index = new_list.map(item => item.name).indexOf(person.name);
         new_list.splice(index, 1);
         setPersons(new_list)
-      }).catch(reason => alert(reason));
+      }).catch(() => setMsg(`${person.name} already deleted from server`, true));
     }
   }
 
@@ -89,6 +110,7 @@ const App = () => {
   return (
     <div>
       <h2>numberbook</h2>
+      <Message message={message}/>
       <Filter values={filter} cb={filterChange}/>
       <DataForm data={person_data} onSubmit={addPerson}/>
       <h2>Numbers</h2>
